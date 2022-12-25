9f72070c4657155b0d8e7d86528920389b7262c7
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2022-12-25 22:08:09 +0900

    feat: READ 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { dbService } from "../fBase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const [neweet, setNeweet] = useState("");
+  const [neweets, setNeweets] = useState([]);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -22,19 +24,47 @@ const Home = () => {
     setNeweet(event.target.value);
   };
 
+  useEffect(() => {
+    let isCancelled = false;
+    (async function fetchDate() {
+      const querySnapshot = await getDocs(collection(dbService, "neweets"));
+      if (!isCancelled) {
+        querySnapshot.forEach((document) => {
+          const neweetsObj = {
+            ...document.data(),
+            id: document.id,
+          };
+          setNeweets((prev) => [neweetsObj, ...prev]);
+        });
+      }
+    })();
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+  console.log(neweets);
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        name=""
-        id=""
-        value={neweet}
-        onChange={onChange}
-        placeholder="What's on your mind?"
-        maxLength={120}
-      />
-      <input type="submit" value="Neweet" />
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={neweet}
+          onChange={onChange}
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Neweet" />
+      </form>
+      <div>
+        {neweets.map((neweet) => (
+          <div key={neweet.id}>
+            <h4>{neweet.neweet}</h4>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 export default Home;
