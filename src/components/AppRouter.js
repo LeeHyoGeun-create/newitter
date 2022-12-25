9c1afef3f17acfd3827cfd649df7fde1c76da8a1
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2022-12-25 21:20:01 +0900

    feat: CREATE 기능 구현

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <BrowserRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />} />
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import { dbService } from "../fBase";
+import { collection, addDoc } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [neweet, setNeweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "neweets"), {
+        neweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    setNeweet("");
+  };
+  const onChange = (event) => {
+    setNeweet(event.target.value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={neweet}
+        onChange={onChange}
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Neweet" />
+    </form>
+  );
+};
 export default Home;
